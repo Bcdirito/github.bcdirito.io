386d61c7167279103c254f89447da5d837a4bbdc
Brian DiRito <bdirito91@gmail.com>
2019-02-18 20:14:09 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -4,29 +4,31 @@ import { connect } from "react-redux"
 import Home from "./components/Home"
 import About from "./components/About"
 import Contact from "./components/Contact"
-import Projects from "./components/Projects"
+import ProjectContainer from "./components/ProjectContainer"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2 className="header">Brian DiRito - Full Stack Developer</h2>
-        <Switch>
-          <Route exact path="/" render={(props) => (
-            <Home {...props}/>
+      <div>
+        <div className="App">
+          <h2 className="header">Brian DiRito - Full Stack Developer</h2>
+          <Switch>
+            <Route exact path="/" render={(props) => (
+              <Home {...props}/>
+              )}/>
+            <Route exact path="/about" render={(props) => (
+              <About {...props}/>
             )}/>
-          <Route exact path="/about" render={(props) => (
-            <About {...props}/>
-          )}/>
-          <Route exact path="/contact" render={(props) => (
-            <Contact {...props}/>
-          )}/>
-          <Route exact path="/projects" render={(props) => (
-            <Projects {...props}/>
-          )}/>
-          <Route component={Home} />
-        </Switch>
+            <Route exact path="/contact" render={(props) => (
+              <Contact {...props}/>
+            )}/>
+            <Route exact path="/projects" render={(props) => (
+              <ProjectContainer {...props}/>
+            )}/>
+            <Route component={Home} />
+          </Switch>
+        </div>
       </div>
     );
   }
--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Projects extends Component {
+export default class ProjectContainer extends Component {
   render() {
     return (
       <div>
