37d11da3ce38b9ad5218036a02a4df768151b34c
Brian DiRito <bdirito91@gmail.com>
2019-02-18 19:20:03 -0500

    added all components to router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Route, Switch, withRouter} from 'react-router-dom'
 import { connect } from "react-redux"
 import Home from "./components/Home"
 import About from "./components/About"
+import Contact from "./components/Contact"
+import Projects from "./components/Projects"
 import './App.css';
 
 class App extends Component {
@@ -16,6 +18,12 @@ class App extends Component {
             )}/>
           <Route exact path="/about" render={(props) => (
             <About {...props}/>
+          )}/>
+          <Route exact path="/contact" render={(props) => (
+            <Contact {...props}/>
+          )}/>
+          <Route exact path="/projects" render={(props) => (
+            <Projects {...props}/>
           )}/>
           <Route component={Home} />
         </Switch>
