31a78cfb606e8915244ec204404be5b012b0b2f1
Brian DiRito <bdirito91@gmail.com>
2019-03-31 18:53:43 -0400

    added styling to resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import resume from "../media/resume.pdf"
-import pic from "../media/resume_pic.jpg"
-import { Document } from "react-pdf"
+import pic from "../media/resume_pic_reduced.jpg"
 
 export default class Resume extends Component {
   render() {
     return (
       <div className="resumeContent">
-        <img className="resume" src={pic} alt="resume" />
-        Click <a href={resume} target="_blank" rel="noopener noreferrer">here</a> to download
+        <div className="resumeRender">
+            <img className="resume" src={pic} alt="resume" />
+        </div>
+        <div className="resumeText">
+            Click <a href={resume} target="_blank" rel="noopener noreferrer">here</a> to download
+        </div>
       </div>
     )
   }
