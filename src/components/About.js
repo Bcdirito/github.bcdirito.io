bf3d441fd47c1a1e90e59bf7f635b66f571895cb
Brian DiRito <bdirito91@gmail.com>
2019-04-03 14:25:14 -0400

    adding content to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import aboutMePhoto from "../media/about_me_photo.jpg"
+import "../css/about.css"
 
 export default class About extends Component {
   render() {
@@ -6,9 +8,12 @@ export default class About extends Component {
       <div>
         <h1>About Me</h1>
         <div className="aboutContent">
-          <p>First paragraph about growing up through college</p>
-          <p>Second paragraph about transition</p>
-          <p>Third Paragraph about Flatiron/developer life</p>
+          <img src={aboutMePhoto} alt="aboutMePhoto"/>
+          <div className="aboutText">
+            <p>First paragraph about growing up through college</p>
+            <p>Second paragraph about transition</p>
+            <p>Third Paragraph about Flatiron/developer life</p>
+          </div>
         </div>
       </div>
     )
