7471c80fe435a0352747ed80f2ca9f90aff911e9
Bcdirito <bdirito91@gmail.com>
2019-10-16 11:28:09 -0400

    fixed hover text error publications

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -28,8 +28,6 @@ export default class Publication extends Component {
                 <img src={kindlingPhoto} alt="kindlingPhoto" className="articleImage" />
                 <p className="description">Staying Inspired during the Job Search Marathon</p>
                 <a href="https://medium.com/@bdirito91/kindling-the-fire-24b80e09907e" target="_blank" rel="noopener noreferrer"><img src={mediumIcon} alt="mediumIcon" onMouseEnter={() => this.enterHandler()} onMouseLeave={() => this.leaveHandler()} id="kindling"/></a>
-                <p className="hoverText">{this.state.hover === true ? this.renderHoverText() : <br/>}
-                {navigator.userAgent.includes("Mobile") === true ? "Tap the icon the read the article" : <br/>}</p>
             </div>
         )
     }
@@ -40,8 +38,6 @@ export default class Publication extends Component {
                 <img className="articleImage" src={exterminatorPhoto} alt="exterminatorPhoto" />
                 <p>Becoming the Champion Debugger</p>
                 <a href="https://medium.com/@bdirito91/call-me-the-exterminator-17b5a579595b" target="_blank" rel="noopener noreferrer"><img src={mediumIcon} alt="mediumIcon" onMouseEnter={() => this.enterHandler()} onMouseLeave={() => this.leaveHandler()}/></a>
-                <p className="hoverText">{this.state.hover === true ? this.renderHoverText() : <br/>}
-                {navigator.userAgent.includes("Mobile") === true ? "Tap the icon the read the article" : <br/>}</p>
             </div>
         )
     }
@@ -52,7 +48,8 @@ export default class Publication extends Component {
             <h2>{this.state.name}</h2>
             {this.props.name === "Kindling the Fire" ? this.renderKindling(): null}
             {this.props.name === "Call Me 'The Exterminator'" ?  this.renderExterminator() : null}
-            <br/>
+            <p className="hoverText">{this.state.hover === true ? this.renderHoverText() : null}
+            {navigator.userAgent.includes("Mobile") === true ? "Tap the icon the read the article" : <br/>}</p>
         </div>
         )
     }
